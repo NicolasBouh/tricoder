5ee2c201d3248221eba75178ea8bbf7c880607cc
Nicolas Bouhours <nico53960@hotmail.fr>
2021-12-09 08:23:48 +0100

    port scanner

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
 mod error;
-mod subdomain;
 mod model;
+mod port;
+mod port_common;
+mod subdomain;
+use std::{env, time::Duration};
+
 pub use error::Error;
 
+use rayon::prelude::*;
+use reqwest::{blocking::Client, redirect};
+
 fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        return Err(Error::CliUsage.into());
+    }
+
+    let target = args[1].as_str();
+
+    let http_timeout = Duration::from_secs(5);
+    let http_client = Client::builder()
+        .redirect(redirect::Policy::limited(4))
+        .timeout(http_timeout)
+        .build()?;
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+
+    pool.install(|| {
+        let scan_result = subdomain::enumerate(&http_client, target);
+    });
 
     Ok(())
 }
